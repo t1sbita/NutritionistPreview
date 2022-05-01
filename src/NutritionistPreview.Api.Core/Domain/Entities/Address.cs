dbdad2c3779667e252c868732f76e303db74fc11
Elias Santos Garcia <elias.garcia@solutis.net.br>
2022-05-01 09:10:20 -0300

    Entidades criadas

---
+++ src/NutritionistPreview.Api.Core/Domain/Entities/Address.cs
@@ -0,0 +1,13 @@
+﻿using NutritionistPreview.Api.Core.Domain.Entities.Base;
+
+namespace NutritionistPreview.Api.Core.Domain.Entities
+{
+    public class Address : BaseEntity
+    {
+        public string Number { get; set; }
+        public long ZipCode { get; set; }
+        public string DescriptionAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+}
